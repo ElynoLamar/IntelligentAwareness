7e2669b008bfea4c81e50e805cec87996a1800ed
Elyno <fernandolamarsantos@gmail.com>
2021-05-12 08:07:00 +0100

    dataset generator

--- code/public/javascripts/dashboard.js
+++ code/public/javascripts/dashboard.js
@@ -1,5 +1,10 @@
+var loggedUser = 0;
 window.onload = function() {
+    document.getElementById('teste').innerHTML = "<input type='button' onclick='teste()' value='CLICK HERE'>  </input>";
+}
 
+function teste() {
+    window.location = 'dataset.html';
 }
 
 function openTasks() {
@@ -7,15 +12,30 @@ function openTasks() {
     block += "<form class='form-container'>";
     block += "<div class='form-content'>";
     block += "<span class='close' onclick='closeMiddleBox()'>&times;</span>";
-    block += "<h1>Tasks for you</h1>";
+    block += "<h1>Notifications for you</h1>";
+    block += "<br><br>";
+    block += "<table>";
+    block += "<tr>";
+    block += "<th>Task name</th><th>Community</th><th>Action</th>";
+    block += "</tr>";
+    block += " <tr>";
+    block += "<td>Peter</td><td>Griffin</td> <td>$100</td>";
+    block += " </tr>";
+    block += "<tr>";
+    block += "<td>Lois</td><td>Griffin</td><td>$150</td>";
+    block += "</tr>";
+    block += "<tr>";
+    block += "<td>Joe</td> <td>Swanson</td> <td>$300</td>";
+    block += "</tr>"
+    block += "<tr>";
+    block += "<td>Cleveland</td><td>Brown</td><td>$250</td>";
+    block += "</tr></table>";
+    block += "<br> <span id='closeButton'>Close</span>";
 
-    block += "<br><br><p> Task nº1 </p>";
-    block += "<p> Task nº2 </p>";
-    block += "<p> Task nº3 </p>";
-    block += "<p> Task nº4 </p>";
     block += "</form>";
     block += "</div>";
     document.getElementById("MiddleBox").innerHTML = block;
+
 
 }
 
@@ -29,9 +49,7 @@ function openNotifs() {
     block += "<div class='form-content'>";
     block += "<span class='close' onclick='closeMiddleBox()'>&times;</span>";
     block += "<h1>Notifications for you</h1>";
-
     block += "<br><br>";
-
     block += "<table>";
     block += "<tr>";
     block += "<th>Task name</th><th>Community</th><th>Action</th>";
---
+++ code/public/javascripts/generateDataset.js
@@ -0,0 +1,149 @@
+window.onload = function() {
+    generate();
+
+}
+
+function generate() {
+    var contador = 1;
+    var numOfEachPersona = 100;
+    var meanAvailableTaskTW = 14
+    var varAvailableTaskTW = 8
+    var availableTaskPW = Math.floor(Math.random() * (meanAvailableTaskTW - varAvailableTaskTW)) + varAvailableTaskTW; // 8 a 13 tasks
+    var availableTaskTW = Math.floor(Math.random() * (meanAvailableTaskTW - varAvailableTaskTW)) + varAvailableTaskTW; // 8 a 13 tasks
+    let block = "";
+    block += "<table>";
+    block += "<tr>";
+    //Prev WEEK
+    block += "<th>ID</th><th>PW: Available Tasks</th><th>PW:Accepted Tasks</th><th>PW:Canceled T.</th><th>PW: Concluded T.</th><th>PW: Times Acessed Platform</th><th>PW: Time spent online</th><th>PW: avg days to conclude T.</th><th>PW: leader previous week?</th><th>PW: leader task Concluded?</th>";
+    //THIS WEEK
+    block += "<th>TW: Available Tasks</th><th>TW: Accepted Tasks</th><th>TW:Canceled T.</th><th>TW: Concluded T.</th><th>TW: Times Acessed Platform</th><th>TW: Time spent online</th><th>TW: avg days to conclude T.</th><th>TW: leader previous week?</th><th>TW: leader task Concluded?</th></tr>";
+    //perfil 1, Com churn e ocupado
+    let acceptedtaskThisweek = 0.51;
+    let canceledtaskThisweek = 0.60;
+    let concludedTaskThisweek = 0.60;
+    let timesAccessedPlatform = 2.4;
+    let timeSpentOnThePlatform = 30; //segundos
+    let avgDaysToConcludeTask = 5;
+    for (let i = 1; i < numOfEachPersona + 1; i++) {
+        block += "<tr>";
+        block += "<td>" + contador++ + "</td>";
+        block += populateTable(availableTaskPW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += populateTable(availableTaskTW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += "</tr>";
+    }
+    //block += "<tr><td></td></tr>"
+    //perfil 2 Com churn e não-ocupado
+    acceptedtaskThisweek = 0.57;
+    canceledtaskThisweek = 0.33;
+    concludedTaskThisweek = 0.72;
+    timesAccessedPlatform = 3.2;
+    timeSpentOnThePlatform = 60; //segundos
+    avgDaysToConcludeTask = 3;
+    for (let i = 1; i < numOfEachPersona + 1; i++) {
+        block += "<tr>";
+        block += "<td>" + contador++ + "</td>";
+        block += populateTable(availableTaskPW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += populateTable(availableTaskTW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += "</tr>";
+    }
+    //block += "<tr><td></td></tr>"
+    //perfil 3 Sem churn mas ocupado
+    acceptedtaskThisweek = 0.75;
+    canceledtaskThisweek = 0.2;
+    concludedTaskThisweek = 0.90;
+    timesAccessedPlatform = 3.9;
+    timeSpentOnThePlatform = 130; //segundos
+    avgDaysToConcludeTask = 2.5;
+    for (let i = 1; i < numOfEachPersona + 1; i++) {
+        block += "<tr>";
+        block += "<td>" + contador++ + "</td>";
+        block += populateTable(availableTaskPW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += populateTable(availableTaskTW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += "</tr>";
+    }
+    //block += "<tr><td></td></tr>"
+    //perfil 4 Sem churn e não ocupado
+    acceptedtaskThisweek = 0.95;
+    canceledtaskThisweek = 0.08;
+    concludedTaskThisweek = 0.90;
+    timesAccessedPlatform = 4.5;
+    timeSpentOnThePlatform = 160; //segundos
+    avgDaysToConcludeTask = 1.5;
+    for (let i = 1; i < numOfEachPersona + 1; i++) {
+        block += "<tr>";
+        block += "<td>" + contador++ + "</td>";
+        block += populateTable(availableTaskPW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += populateTable(availableTaskTW, acceptedtaskThisweek, canceledtaskThisweek, concludedTaskThisweek, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask)
+        block += "</tr>";
+    }
+    block += "</table>";
+    block += "</div>";
+    document.getElementById('dataset').innerHTML = block;
+
+}
+
+function populateTable(availableTask, acceptedtask, canceledtask, concludedTask, timesAccessedPlatform, timeSpentOnThePlatform, avgDaysToConcludeTask) {
+    let block = "";
+    block += "<td>" + availableTask + "</td>";
+    let thisacceptedtaskPerc = getRandomIntInclusive(acceptedtask - 0.10, acceptedtask + 0.10);
+    let thisacceptedtask = Math.round(thisacceptedtaskPerc * availableTask)
+    if (thisacceptedtask > availableTask) block += "<td>" + availableTask + " </td>"; // prob de aceitar a task
+    else block += "<td>" + thisacceptedtask + " </td>"; // prob de aceitar a task
+
+    let thiscanceledtaskPerc = getRandomIntInclusive(canceledtask - 0.10, canceledtask + 0.10);
+    let thiscanceledtask = Math.ceil(thisacceptedtask * thiscanceledtaskPerc)
+    block += "<td>" + thiscanceledtask + " </td>"; // prob de cancelar a task
+
+    let thisconcludedTaskPerc = getRandomIntInclusive(concludedTask - 0.10, concludedTask + 0.10);
+    let thisconcludedTask = Math.floor((thisacceptedtask - thiscanceledtask) * thisconcludedTaskPerc);
+    block += "<td>" + thisconcludedTask + "</td>"; // prob de concluir a task
+
+    let thisTimesAccessed = getRandomIntInclusive(timesAccessedPlatform - 2, timesAccessedPlatform + 3);
+    block += "<td>" + thisTimesAccessed + " times</td>"; // Times accessed platform
+    block += "<td>" + getRandomIntInclusiveCeil(timeSpentOnThePlatform * timesAccessedPlatform - 30, timeSpentOnThePlatform * timesAccessedPlatform + 300) + " seconds</td>"; // time spent per access on avg
+    block += "<td>" + getRandomIntInclusiveCeil(avgDaysToConcludeTask - 1, avgDaysToConcludeTask + 1) + " days</td>"; // avg day to conclude a task
+
+    let isLeader = checkLeader(availableTask, thisacceptedtask, thiscanceledtask, thisconcludedTask);
+    block += "<td>" + isLeader + "</td>"; // was previous leader?
+    block += "<td>" + leaderTaskConcluded(thisconcludedTask, isLeader) + "</td>"; // prob of doing leader task
+    return block;
+}
+
+function getRandomIntInclusive(min, max) {
+    console.log(min);
+    console.log(max);
+    return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+
+function getRandomIntInclusiveCeil(min, max) {
+    return Math.ceil(Math.random() * (max - min) + min); //The maximum is inclusive and the minimum is inclusive
+}
+
+
+function checkLeader(total, accepted, canceled, completed) {
+    let answer = "";
+    let x = (accepted - canceled) / total
+    let y = completed / accepted
+    if (x > 0.55) { // if the accepted tasks are higher than 55%
+        if (y > 0.70) { // if the completed chance is higher than 70%
+            answer = "Leader";
+        } else {
+            answer = "Normal";
+        }
+    } else if (y < 0.25) {
+        answer = "Anchor";
+    } else {
+        answer = "Normal";
+    }
+    return answer;
+}
+
+function leaderTaskConcluded(concludedChance, lead) {
+    let x = getRandomIntInclusive(concludedChance - 0.10, concludedChance + 0.10);
+    if (lead == "Leader") {
+        if (x >= concludedChance) {
+            return "Yes";
+        } else return "No";
+    } else return "No";
+}
